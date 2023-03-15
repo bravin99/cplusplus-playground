80ff4e6d7da53dac21d2abc556388c9113ef7544
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-03-15 08:36:53 +0300

    snackbar pos

--- snackbar_pos.cpp
+++ snackbar_pos.cpp
@@ -1,21 +1,29 @@
 #include<iostream>
+#include<list>
 using namespace std;
 
 int menu_ids[] {1,2,3,4,5,6};
 string menu[] {"Hot Dog", "Hamburger", "French Fries", "Milk Shake", "Coffee", "Beef Smokies"};
 double menu_prices[] {90.0, 300.0, 150.0, 150.0, 70.0, 40.0};
+double subtotal = 0;
+double vat_percentage = 16;
+double vat = 0;
+double grandtotal = 0;
+double amount_tendered = 0;
+double change = 0;
 
 struct cartitem {
     int menu_id;
     int quantity;
 } c_item;
 
-cartitem busket[] {};
+list<cartitem> busket;
 
 int client_input = 1;
 
 void get_menu();
 void sell_items();
+void calculate_total();
 
 int main()
 {
@@ -33,7 +41,16 @@ int main()
         }
         get_menu();
     }
-    cout<<"Size of busket: "<<busket[0].menu_id<<busket[0].quantity;
+
+    if (subtotal > 0)
+    {
+        calculate_total();
+        cout<<"Enter amount tendered for payment: ";
+        cin>>amount_tendered;
+        change = amount_tendered - grandtotal;
+        cout<<"Change: "<<change<<"\n"<<endl;
+    }
+    cout<<"\t \t THANK YOU AND COME AGAIN \n"<<endl;
 }
 
 void get_menu()
@@ -49,16 +66,24 @@ void get_menu()
 
 void sell_items()
 {
-    cout<<"\n";
-    cout<<"Enter Item No: ";
+    cout<<"\nEnter Item No: ";
     cin>>c_item.menu_id;
-    cout<<"\n";
-    cout<<"Enter Quantity: ";
+    cout<<"\nEnter Quantity: ";
     cin>>c_item.quantity;
-    cout<<"\n";
+    // Add to busket
+    busket.push_back(c_item);
+    cout<<"Item added to cart"<<endl;
+    //subtotal
+    subtotal += menu_prices[(c_item.menu_id)-1] * c_item.quantity;
+    cout<<"Subtotal: KES "<<subtotal<<"\n";
     cout<<"Press 0 to exit or any other key to add more items."<<endl;
     cin>>client_input;
+}
 
-    // TODO: Add to busket
-
+void calculate_total()
+{
+    vat = subtotal * (vat_percentage/100);
+    grandtotal = subtotal + vat;
+    cout<<"\nVAT at "<<vat_percentage<<" percent: "<<vat<<"\n";
+    cout<<"Total Amount: "<<grandtotal<<"\n";
 }
