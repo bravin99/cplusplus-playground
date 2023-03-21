c9523cdd6ea4cf2b083d44e2f4a1417966217166
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-03-21 22:07:36 +0300

    classes exercises

--- classes_exercise1.cpp
+++ classes_exercise1.cpp
@@ -1,7 +1,9 @@
 /*
     Write a class having two private variables and one member function which will return the area of the rectangle.
 */
-#include<iostream>>
+#include<iostream>
+#include<sstream>
+
 using namespace std;
 
 class Area
@@ -19,10 +21,18 @@ public:
 
 int main()
 {
+    string myString;
     Area rect1;
-    rect1.length = 20;
-    rect1.width = 10;
-    cout<<"Area of rect1 is: "<<rect1.calculate_area()<<endl;
+    cout<<"Calculating Area of Triangles\n"<<endl;
+    cout<<"Enter length: ";
+    getline(cin,myString);
+    stringstream(myString)>>rect1.length;
+    cout<<"\n"<<endl;
+    cout<<"Enter width: ";
+    getline(cin,myString);
+    stringstream(myString)>>rect1.width;
+    cout<<"\n"<<endl;
+    cout<<"The area of the rectagle is: "<<rect1.calculate_area()<<endl;
 
     cout<<"\n"<<endl;
 
