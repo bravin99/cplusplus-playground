93a45e3fe0442d625998fad0a4c7f772e21c9cc1
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-03-21 22:19:32 +0300

    classes exercises

---
+++ classes_exercise2.cpp
@@ -0,0 +1,41 @@
+/*
+    Write a program and input two integers in main and pass them to default constructor of the class.
+    Show the result of the additon of two numbers.
+*/
+
+#include<iostream>
+#include<sstream>
+
+using namespace std;
+
+class ExampleConstructor
+{
+public:
+    int val1, val2;
+    ExampleConstructor(int a, int b)
+    {
+        val1 = a;
+        val2 = b;
+        cout<<"Initialized value1="<<val1<<", and value2="<<val2<<endl;
+    }
+};
+
+int main()
+{
+    string myString;
+    int a,b;
+
+    cout<<"Enter the first integer: ";
+    getline(cin,myString);
+    stringstream(myString)>>a;
+    cout<<"\n";
+
+    cout<<"Enter the second integer: ";
+    getline(cin,myString);
+    stringstream(myString)>>b;
+    cout<<"\n";
+
+    ExampleConstructor(a,b);
+    cout<<"\n"<<endl;
+
+}
