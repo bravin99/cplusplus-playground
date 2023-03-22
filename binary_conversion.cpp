88abaf2c42297fc1d7c786f535aa2b5f45620af4
Bravin Shumwe <bravinshumwe.coding@gmail.com>
2023-03-22 10:13:30 +0300

    binary conversion exercise

---
+++ binary_conversion.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+string convert_to_binary(int a)
+{
+    string result;
+    while (a != 0)
+    {
+        result = (a%2==0 ? "0" : "1")+result;
+        a /= 2;
+    }
+    return result;
+}
+
+int main()
+{
+    int n = 9;
+    cout<<"binary for "<<n<<", is "<<convert_to_binary(n)<<"\n"<<endl;
+
+}
